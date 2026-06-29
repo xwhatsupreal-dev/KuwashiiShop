a9ea728ec4078af39f49abac021070798210ad2b
xwhatsupreal-dev <xwhatsupreal@gmail.com>
2026-06-29 16:35:09 +0700

    Delete fix-logout-app.cjs

--- fix-logout-app.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf-8');
-
-code = code.replace(/logout\(\);\n *setIsAstdMenuOpen/g, "handleLogout();\n                                  setIsAstdMenuOpen");
-
-fs.writeFileSync('src/App.tsx', code);
-console.log('Fixed logout bug');
