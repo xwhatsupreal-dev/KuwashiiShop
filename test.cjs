8538c85ae82f35009a6b02e7b3280d4889378d10
xwhatsupreal-dev <xwhatsupreal@gmail.com>
2026-07-07 16:56:09 +0700

    chore: clean up temporary scripts and archive files

--- test.cjs
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.get('http://localhost:3000/api/version', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => console.log(data));
-});
--- test_api2.ts
+++
@@ -1,9 +0,0 @@
-async function test() {
-  try {
-    const res = await fetch('https://www.wondd.com/game/garena/index.php');
-    const text = await res.text();
-    console.log("HTML length:", text.length);
-    console.log("Excerpt:", text.slice(0, 1000));
-  } catch (e) { console.error(e); }
-}
-test();
