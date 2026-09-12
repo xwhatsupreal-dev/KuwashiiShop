fb39ee8858c862388d505e104f73e087bf6a50b3
xwhatsupreal-dev <xwhatsupreal@gmail.com>
2026-09-12 15:52:58 +0700

    style: update popular badge icon and dimensions
    Replace the popular fire badge asset and adjust its size and positioning for better visual alignment on item cards.

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -97,9 +97,9 @@ export const ItemCard: React.FC<ItemCardProps> = ({
       {/* Popular Fire Badge on the card edge */}
       {item.isPopular && (
         <img 
-          src="https://img2.pic.in.th/1000121599.png" 
+          src="https://img2.pic.in.th/1000121600.png" 
           alt="Hot" 
-          className="absolute -top-3 -right-3 w-10 h-10 sm:w-12 sm:h-12 z-40 drop-shadow-[0_0_8px_rgba(255,100,0,0.8)] pointer-events-none" 
+          className="absolute -top-2 -right-2 sm:-top-2.5 sm:-right-2.5 w-8 h-8 sm:w-10 sm:h-10 z-40 drop-shadow-[0_0_8px_rgba(255,100,0,0.8)] pointer-events-none" 
         />
       )}
 
