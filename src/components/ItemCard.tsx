0b9e4a47020bc48c51f900acea7b51acb31cbe11
xwhatsupreal-dev <xwhatsupreal@gmail.com>
2026-09-12 17:03:55 +0700

    feat(ui): add pulse animation to popular item badge

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -99,7 +99,7 @@ export const ItemCard: React.FC<ItemCardProps> = ({
         <img 
           src="https://cdn.phototourl.com/free/2026-09-12-946d23d8-794c-40de-9edd-a10ac9b4df7e.png" 
           alt="Hot" 
-          className="absolute -top-6 -right-6 w-20 h-20 sm:-top-8 sm:-right-8 sm:w-24 sm:h-24 z-40 drop-shadow-[0_0_8px_rgba(255,100,0,0.8)] pointer-events-none" 
+          className="absolute -top-6 -right-6 w-20 h-20 sm:-top-8 sm:-right-8 sm:w-24 sm:h-24 z-40 drop-shadow-[0_0_8px_rgba(255,100,0,0.8)] pointer-events-none animate-pulse" 
         />
       )}
 
